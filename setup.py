a260bdca45e629eb76b2def404554ec6e51d9435
prakhardwivedi82 <prakhardwivedi82@gmail.com>
2025-09-23 18:10:23 +0530

    requirements and setup done

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements() -> List[str]:
+    """Reads the requirements from a file and returns them as a list."""
+
+    requirement_lst:List[str]=[]
+
+    try:
+        with open('requirements.txt','r') as file:
+            lines=file.readlines()
+            for line in lines:
+                requirement=line.strip()
+                # ignore empty lines and -e .
+                if requirement and requirement != '-e .':
+                    requirement_lst.append(requirement)
+    
+    except FileNotFoundError:
+        print("requirements.txt file not found.")
+
+    return requirement_lst
+
+print(get_requirements())
